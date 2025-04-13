83fc181f818cdb8ee899cd4121ad16832e2950cc
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2025-04-13 05:02:43 +0100

    feat: modified puma rb file

--- config/puma.rb
+++ config/puma.rb
@@ -39,3 +39,9 @@ plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+# Use multiple processes (workers) in production
+workers ENV.fetch("WEB_CONCURRENCY", 2)
+
+# Preload the app before forking for memory efficiency
+preload_app!
